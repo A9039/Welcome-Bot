d69d9e7aedd3910192e461225949fbd5ece68c21
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-06-11 02:27:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require("http");
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
+
 /**
  * Module Imports
  */
